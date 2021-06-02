2b7356fe66184be081ab90cabd725be127607759
Nazri <abienazri23@gmail.com>
2021-06-02 17:22:33 +0700

    menambahkan lingkarang pada jari telunjut

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -27,6 +27,9 @@ while True:
                 print(id, cx, cy)
                 if id == 4:
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
